f6bdb76e9d10f0e121636203b60c21f0558e4a71
Ligia Monteiro <103998115+IamGrootie@users.noreply.github.com>
2022-06-20 17:43:52 +0100

    Update Body.js

--- travel-journal/src/components/Body.js
+++ travel-journal/src/components/Body.js
@@ -19,14 +19,3 @@ export default function Body(props) {
     )
 }
 
-        // <div className="card">
-        //     {props.openSpots>0 ? <p className="card--spots">Online</p> : <p className="card--spots">SOLD OUT</p>}
-        //     <img src={`../images/${props.coverImg}`} className="profile--pic"/>
-        //     <div className="card--details">
-        //         <img src={Star} className="card--star"/>
-        //         <p>{props.stats.rating}</p>
-        //         <p className="card--gray"> ({props.stats.reviewCount}) • {props.location}</p>
-        //     </div>
-        //     <p className="card--description">{props.title}</p>
-        //     <p className="card--price"><span className="bold">From ${props.price}</span> / person</p>
-        // </div>
